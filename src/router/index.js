d76bff3b0b0d137526a6c11b7d82fa5e4e25340e
Marina Pak <marrinna03@mail.ru>
2025-03-29 06:21:48 +0300

    Защита маршрутов

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import TasksPage from '@/pages/TasksPage.vue';
 import AppFeedback from '@/components/AppFeedback.vue';
 import RegistrationPage from '@/pages/RegistrationPage.vue'
+import { useAuthStore } from '@/stores/firebase/AuthStore';   
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,17 +14,31 @@ const router = createRouter({
     {
       path: '/auth',
       component: RegistrationPage,
+      
     },
     {
       path: '/tasks/:filter',
       component: TasksPage,
       props: true,
+      meta: { requiresAuth: true }
     },
     {
       path: '/feedback',
       component: AppFeedback,
+      meta: { requiresAuth: true }
     },
   ],
 });
+
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore();
+
+  if (to.meta.requiresAuth && !authStore.user) {
+    next('/auth'); 
+  } else {
+    next(); 
+  }
+});
+
 
 export default router;
